d711027ceb290cc874624b59f89a3267653bf202
xdezark <sebahertzogg@gmail.com>
2022-08-28 15:29:00 -0300

    se agrega

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
-let entrada= prompt("Selecciona un producto \n 1-Papa $20 \n 2-Tomate $30 \n 3-Lechuga $15 \n 4-Zapallito $12 \n 5-Ningun producto" );
-let productos = "";
+function seleccionaProducto(){
+    let seleccion;
+    do{
+    seleccion = parseInt(prompt("Selecciona un producto: \n1 Papa \n2 Tomate \n3 Zapallito \n4 Huevos"));
+    }while(seleccion != 1 && seleccion !=2 && seleccion !=3 && seleccion !=4);
 
-while (entrada !="5"){
-productos += entrada +"\n"
-entrada = prompt("Selecciona un producto \n 1-Papa $20 \n 2-Tomate $30 \n 3-Lechuga $15 \n 4-Zapallito $12 \n 5-Ningun producto" );
+
+switch(seleccion){
+    case 1:
+    return "Papa";
+    case 2:
+    return "Tomate";
+    case 3:
+    return "Zapallito";
+    case 4:
+    return "Huevos";
 }
-console.log(productos);
+}
+
+function validarPrecio(producto){
+    if(producto==="Papa"){
+        return 100;
+    }
+    else if (producto==="Tomate"){
+        return 300;
+    }
+    else if (producto==="Zapallito"){
+        return 250;
+    }
+    else{
+        return 1000;
+    }
+}
+
+function cobrar(nombre,precio){
+alert("Seleccionaste "+ nombre+"\nPrecio: $"+precio);
+let pago= parseInt(prompt("Con cuanto pagas?"))
+if(pago>precio){
+    alert("Gracias! Tu vuelto es "+ (pago - precio))
+}else{
+    alert("El monto ingresado es insuficiente")
+}
+}
+
+
+
+let productoSelect = seleccionaProducto();
+let precioProducto = validarPrecio(productoSelect);
+cobrar(productoSelect,precioProducto);
+
+seleccionaProducto();
+ 
